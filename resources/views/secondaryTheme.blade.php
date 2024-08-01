02857ea8d728cf370d52e82b6e82e1d2a0b6e33c
riddhielsnerr2102 <riddhi@elsner.in>
2024-08-01 20:36:33 +0530

    routes updated

--- resources/views/secondaryTheme.blade.php
+++ resources/views/secondaryTheme.blade.php
@@ -25,10 +25,10 @@
         </div>
         <div class="menus">
             <span class="menu-name"><a href="/#services">SERVICES</a></span>
-        <span class="menu-name"> <a href="portfolio">PORTFOLIO</a></span>
+        <span class="menu-name"> <a href="{{route('portfolio')}}">PORTFOLIO</a></span>
                 <!-- <span class="menu-name">BLOG</span> -->
-            <span class="menu-name"><a href="about-us">ABOUT US</a></span>
-            <span class="menu-name"><a href="contact-us">CONTACT US</a></span>
+            <span class="menu-name"><a href="{{route('aboutUs')}}">ABOUT US</a></span>
+            <span class="menu-name"><a href="{{route('contactUs')}}">CONTACT US</a></span>
 
 
         </div>
@@ -49,10 +49,10 @@
         </div>
         <div class="menus-sidebar">
         <div class="menu-name-sidebar"><a href="/#services">SERVICES</a></div>
-             <div class="menu-name-sidebar"> <a href="portfolio">PORTFOLIO</a></div>
+             <div class="menu-name-sidebar"> <a href="{{route('portfolio')}}">PORTFOLIO</a></div>
           <!--  <div class="menu-name-sidebar">BLOG</div> -->
-            <div class="menu-name-sidebar"><a href="about-us">ABOUT US</a></div>
-            <div class="menu-name-sidebar"><a href="contact-us">CONTACT US</a></div>
+            <div class="menu-name-sidebar"><a href="{{route('aboutUs')}}">ABOUT US</a></div>
+            <div class="menu-name-sidebar"><a href="{{route('contactUs')}}">CONTACT US</a></div>
             <!-- <div class="menu-name-sidebar">SERVICES</div>
             <div class="menu-name-sidebar">PORTFOLIO</div>
             <div class="menu-name-sidebar">BLOG</div>
@@ -82,8 +82,8 @@
             <div class="what-we-do">
                 <div class="footer-header">About Krishiv</div>
                 <div class="footer-content">
-                    <a href="about-us">About us</a>
-                    <a href="portfolio">Portfolio</a>
+                    <a href="{{route('aboutUs')}}">About us</a>
+                    <a href="{{route('portfolio')}}">Portfolio</a>
                     <!-- <a href="#">Blog</a>
                     <a href="#">Terms and Conditions</a> -->
 
